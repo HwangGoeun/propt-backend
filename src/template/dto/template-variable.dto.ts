8a70a6dc023c8dae7617efc5f9dd431a0e9f6c3c
HwangGoeun <hwang_gim@naver.com>
2026-01-11 01:06:26 +0900

    refactor: 기획 변경에 따른 필드(type, allowedExtensions) 제거

--- src/template/dto/template-variable.dto.ts
+++ src/template/dto/template-variable.dto.ts
@@ -1,6 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsArray, IsIn, IsNotEmpty, IsOptional, IsString } from 'class-validator';
-import type { VariableType } from 'src/common/types/template.types';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class TemplateVariableDto {
   @ApiProperty({ example: 'keyword' })
@@ -8,18 +7,8 @@ export class TemplateVariableDto {
   @IsNotEmpty()
   name!: string;
 
-  @ApiProperty({ enum: ['text', 'file'], example: 'text' })
-  @IsIn(['text', 'file'])
-  type!: VariableType;
-
   @ApiProperty({ required: false, example: 'Please enter your search keyword' })
   @IsOptional()
   @IsString()
   description?: string;
-
-  @ApiProperty({ required: false, example: ['pdf', 'docx'] })
-  @IsOptional()
-  @IsArray()
-  @IsString({ each: true })
-  allowedExtensions?: string[];
 }
